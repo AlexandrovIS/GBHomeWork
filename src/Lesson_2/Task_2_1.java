d5a16dc13143a4eeb44e0ef3fd6e51e027d4b45d
alexandrovis <Alexandrovis@gmail.com>
2020-08-28 18:15:04 +0300

    Отправляем 1-6 задания Второго урока на ветку Lesson2

---
+++ src/Lesson_2/Task_2_1.java
@@ -0,0 +1,12 @@
+package Lesson_2;
+
+public class Task_2_1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==1) arr[i]=0;
+            else arr[i]=1;
+            System.out.print(arr[i]);
+        }
+    }
+}
